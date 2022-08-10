ea127cc0d3e33e56e20caac79010932029822c82
rosh2707b <74601892+rosh2707b@users.noreply.github.com>
2022-08-10 15:37:17 +0530

    Update git-triggered.sh

--- pipelines/git-triggered.sh
+++ pipelines/git-triggered.sh
@@ -1,14 +1,13 @@
-#! /bin/sh
-config:
-      inputs:
-        - name: concourse-docs-git
-      platform: linux
-      image_resource:
-        type: registry-image
-        source: { repository: busybox }
-      run:
-        path: /bin/sh
-        args: 
-          - -exc
-          - |
-            ls concourse-docs-git/tasks
+---
+platform: linux
+image_resource:
+    type: registry-image
+    source: { repository: busybox }
+inputs:
+    - name: concourse-docs-git
+run:
+    path: /bin/sh
+    args: 
+      - -exc
+      - |
+        ls concourse-docs-git/tasks
